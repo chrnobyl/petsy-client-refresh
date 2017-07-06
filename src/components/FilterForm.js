8c31053836a0bb9228e212c77567c0d69013679d
github username <github email address>
2017-07-06 18:53:37 -0400

    cities added to form

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -11,31 +11,37 @@ const species = [
   { key: 'd', text: 'Dog', value: 'dog'}
 ]
 
-let cities = props.shelters.map
 
-const FilterForm = (props) => (
-  <Modal trigger={<Button>Show User Preferences</Button>}>
-    <Modal.Header>Filter pets by:</Modal.Header>
-    <Modal.Content image>
-      <Form size='big'>
-        <Form.Group widths='equal'>
-          <Form.Select label='Species' options={species} placeholder='Species' />
-          <Form.Select label='Sex' options={sexes} placeholder='Sex' />
-          <Form.Select label='City' options={cities} placeholder='City' />
-          <Form.Field>
-                <Checkbox label='Small' />
-                <Checkbox label='Medium' />
-                <Checkbox label='Large' />
-          </Form.Field>
-          <Form.Field label='Zip code' control='input' placeholder='Zip code' />
-        </Form.Group>
 
-        <Form.Button>Submit</Form.Button>
-      </Form>
-    </Modal.Content>
-  </Modal>
-)
+const FilterForm = (props) => {
+  debugger
+  let cities = props.shelters.map((shelter, i) => {
+    return { key: i, text: shelter.city, value: shelter.city }
+  })
 
+  return (
+    <Modal trigger={<Button>Show User Preferences</Button>}>
+      <Modal.Header>Filter pets by:</Modal.Header>
+      <Modal.Content image>
+        <Form size='big'>
+          <Form.Group widths='equal'>
+            <Form.Select label='Species' options={species} placeholder='Species' />
+            <Form.Select label='Sex' options={sexes} placeholder='Sex' />
+            <Form.Select label='City' options={cities} placeholder='City' />
+            <Form.Field>
+                  <Checkbox label='Small' />
+                  <Checkbox label='Medium' />
+                  <Checkbox label='Large' />
+            </Form.Field>
+            <Form.Field label='Zip code' control='input' placeholder='Zip code' />
+          </Form.Group>
+
+          <Form.Button>Submit</Form.Button>
+        </Form>
+      </Modal.Content>
+    </Modal>
+  )
+}
 export default FilterForm
         //   <h3>Pet Filter</h3>
         //   <form onSubmit={console.log("submitted")}>
