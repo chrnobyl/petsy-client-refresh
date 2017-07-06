4853b0ec934bcbf14a17ce4729f09c691591585e
github username <github email address>
2017-07-06 18:44:29 -0400

    built out form

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -1,19 +1,37 @@
 import React from 'react'
-import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import { Button, Header, Image, Modal, Form, Checkbox } from 'semantic-ui-react'
+
+const sexes = [
+  { key: 'm', text: 'Male', value: 'male' },
+  { key: 'f', text: 'Female', value: 'female' },
+]
+
+const species = [
+  { key: 'c', text: 'Cat', value: 'cat'},
+  { key: 'd', text: 'Dog', value: 'dog'}
+]
+
+let cities = props.shelters.map
 
 const FilterForm = (props) => (
   <Modal trigger={<Button>Show User Preferences</Button>}>
     <Modal.Header>Filter pets by:</Modal.Header>
     <Modal.Content image>
-      <form onSubmit={console.log("hi")}>
-        <select name="category" value={props.selected}>
-          <option value="1">Male</option>
-          <option value="2">Female</option>
-          <option value="3">Dog</option>
-          <option value="4">Cat</option>
-        </select>
-        <input type="submit" value="Submit" />
-      </form>
+      <Form size='big'>
+        <Form.Group widths='equal'>
+          <Form.Select label='Species' options={species} placeholder='Species' />
+          <Form.Select label='Sex' options={sexes} placeholder='Sex' />
+          <Form.Select label='City' options={cities} placeholder='City' />
+          <Form.Field>
+                <Checkbox label='Small' />
+                <Checkbox label='Medium' />
+                <Checkbox label='Large' />
+          </Form.Field>
+          <Form.Field label='Zip code' control='input' placeholder='Zip code' />
+        </Form.Group>
+
+        <Form.Button>Submit</Form.Button>
+      </Form>
     </Modal.Content>
   </Modal>
 )
--- src/components/UserPets.js
+++ src/components/UserPets.js
@@ -1,84 +1,34 @@
-// import React, { Component } from 'react'
-// import { Link } from 'react-router-dom'
-// import { Image, List, Dimmer, Loader, Segment  } from 'semantic-ui-react'
-// import PetAdapter from '../adapters/PetAdapter'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { Image, List, Dimmer, Loader, Segment  } from 'semantic-ui-react'
+import PetAdapter from '../adapters/PetAdapter'
 // import PetDetail from './PetDetail'
 
-import React, { Component } from 'react'
-import { Grid, Menu, Segment, Button } from 'semantic-ui-react'
-import '../App.css';
-
-export default class PetDetail extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      activeItem: '',
-      pets: []
-     }
-
-    }
-    componentDidReceiveProps(props){
-      this.setState({
-        pets: props.pets
-      })
-    }
-
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  render() {
-    debugger
-    const { activeItem } = this.state
-
+export default function UserPets(props) {
+  if (props.pets.length >= 1) {
+  return (
+    <div className="right">
+      <List animated verticalAlign='middle' size="massive">
+        {props.pets.map((pet, i) => {
+          return (
+            <List.Item key={i} as={ Link } to={`/pets/${pet.id}`} >
+              <Image avatar src={pet.picture} />
+              <List.Content>
+                <List.Header>
+                  {pet.name}
+                </List.Header>
+              </List.Content>
+            </List.Item>
+          )
+        })}
+      </List>
+    </div>
+  )
+  } else {
     return (
-      <Grid>
-          <Grid.Column stretched width={12}>
-            {this.state.pets.map(pet => {
-            <Segment>
-              {pet.name}, Age {pet.age}
-              <img src={pet.picture} />
-              {pet.description}
-              <Button onClick={() => this.props.deleteUserPet(pet.id)}>Remove pet from list</Button>
-            </Segment>
-            })}
-        </Grid.Column>
-        <Grid.Column width={4}>
-          <Menu fluid vertical tabular='right'>
-            {this.state.pets.map(pet => {
-            <Menu.Item name={pet.name} active={activeItem === pet.name} onClick={this.handleItemClick} />
-          })}
-          </Menu>
-        </Grid.Column>
-      </Grid>
+      <div className="right">
+        <h2>Choose a pet to get started.</h2>
+      </div>
     )
   }
 }
-
-// export default function UserPets(props) {
-//   if (props.pets.length >= 1) {
-//   return (
-//     <div className="right">
-//       <List animated verticalAlign='middle' size="massive">
-//         {props.pets.map((pet, i) => {
-//           return (
-//             <List.Item key={i} as={ Link } to={`/pets/${pet.id}`} >
-//               <Image avatar src={pet.picture} />
-//               <List.Content>
-//                 <List.Header>
-//                   {pet.name}
-//                 </List.Header>
-//               </List.Content>
-//             </List.Item>
-//           )
-//         })}
-//       </List>
-//     </div>
-//   )
-//   } else {
-//     return (
-//       <div className="right">
-//         <h2>No pets yet!</h2>
-//       </div>
-//     )
-//   }
-// }
