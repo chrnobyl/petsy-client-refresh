bb61ba462daa12c5c972cb4ede48f240f601face
github username <github email address>
2017-08-31 09:58:26 -0400

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// var express = require('express');
-// var app = express();
-//
-// app.use(express.static(__dirname + '/'));
-//
-// app.listen(process.env.PORT || 8080);
+var express = require('express');
+var app = express();
 
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(process.env.PORT || 5000, function(){
-    console.log('Server running on 5000...');
-});
+app.use(express.static(__dirname + '/'));
+
+app.listen(process.env.PORT || 8080);
+
+// var connect = require('connect');
+// var serveStatic = require('serve-static');
+// connect().use(serveStatic(__dirname)).listen(process.env.PORT || 5000, function(){
+//     console.log('Server running on 5000...');
+// });
